e94cab138f3348d86d8490a1de0326598e80ea1f
Manthri Saikishore <101115191+saikishoreMSK@users.noreply.github.com>
2025-09-09 12:18:38 +0530

    Update App.jsx url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import { snippets } from './components/snippets';
 
-const socket = io('http://localhost:5000');
+const socket = io("https://realtime-code-editor-fdvk.onrender.com");
 
 const App = () => {
   const [Joined, setJoined] = useState(false);
