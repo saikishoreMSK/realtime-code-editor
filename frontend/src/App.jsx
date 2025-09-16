f7f39982ab4c7944ecee6873cfa867b5dc9f5e70
M Saikishore <manthrisaikishore@gmail.com>
2025-09-16 11:38:55 +0530

    Testing Application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import { snippets } from './components/snippets';
 
-const socket = io("https://realtime-code-editor-1-wi4f.onrender.com");
+const socket = io("http://localhost:5000");
 
 const App = () => {
   const [Joined, setJoined] = useState(false);
