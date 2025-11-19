e7a57ee8d405e1acf6a1a5c49d7f434da8897391
Manthri Saikishore <101115191+saikishoreMSK@users.noreply.github.com>
2025-11-19 10:08:26 +0530

    Change socket connection URL to localhost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import { snippets } from './components/snippets';
 
-const socket = io("https://realtime-code-editor-65ch.vercel.app/");
+//const socket = io("https://realtime-code-editor-65ch.vercel.app/");
+const socket = io("http://localhost:5000");
 
 const App = () => {
   const [Joined, setJoined] = useState(false);
